3a1391908c1a0f1cd68159a2323cd57a30f7d525
kbj2285 <31677723+kbj2285@users.noreply.github.com>
2017-09-06 14:59:24 +0900

    Add files via upload

---
+++ kbj2285.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+printf("student id number = 2017301011\n");
+
+printf("Your name = ±è¹üÁØ\n");
+
+printf("Your mobile phone number = 01055952285\n");
+
+printf("Your GitHub address = kbj2285\n");
+
+
+return 0;
+
+}
